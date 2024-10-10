3cb40473f14f2cb1abc3437ab7f7c3ceb940ba43
Palle Cogburn <palle@4thex.com>
2024-10-10 06:29:40 +0200

    Sorts vertices counterclockwise and calculates the area.

--- spec/triangle.spec.js
+++ spec/triangle.spec.js
@@ -1,10 +1,6 @@
 import { triangle } from 'strands';
 describe('triangle', () => {
-    it('works', () => {
-        let t1 = triangle();
-        expect(t1.size).toBe(6);
-    });
-    it('orders vertices', () => {
+    it('size returns the area of the triangle', () => {
         let t1 = triangle({
             vertices: [
                 { x: 0, y: 0 },
@@ -12,14 +8,23 @@ describe('triangle', () => {
                 { x: 2, y: 0 }
             ]
         });
-        let vertices = t1.vertices;
-        // How do I detect that the vertices are returned counterclockwise?
-        // Answer: ???
-        expect(vertices[0]).toEqual(
-            [
+        expect(t1.size).toBe(2);
+    });
+    it('sort returns vertices counterclockwise', () => {
+        let t1 = triangle({
+            vertices: [
                 { x: 0, y: 0 },
                 { x: 0, y: 2 },
                 { x: 2, y: 0 }
+            ]
+        });
+        let vertices = t1.sort();
+        
+        expect(vertices).toEqual(
+            [
+                { x: 0, y: 0 },
+                { x: 2, y: 0 },
+                { x: 0, y: 2 }
             ]
         );
     });
--- src/triangle/triangle.js
+++ src/triangle/triangle.js
@@ -1,24 +1,35 @@
 const triangle = (conf) => {
+    let vertices = conf.vertices;
+
     const size = {
         get value() {
-            return 5;
+            let v = sort();
+            let result = Math.abs(v[0].x*(v[1].y-v[2].y)+v[1].x*(v[2].y-v[0].y)+v[2].x*(v[0].y-v[1].y))/2;
+            return result;
         }
     };
 
-    const vertices = {
-        elements: [],
-        get value() {
-            return vertices;
-        },
-        set value(value) {
-            elements = value;
-        }
-    };
 
-    vertices.value = conf.vertices;
+    const sort = () => {
+        let result = vertices.slice();
+        let p1 = vertices[0];
+        let p2 = vertices[1];
+        let p3 = vertices[2];
+        
+        // Calculate slope between p1 and p2
+        let a1 = (p2.y-p1.y)/(p2.x-p1.x);
+        // Calculate slope between p1 and p3
+        let a2 = (p3.y-p1.y)/(p3.x-p1.x);
+        if(a1 > a2) {
+            result[1] = p3;
+            result[2] = p2;
+        }
+        return result;
+    };
 
     return {
-        size: size.value
+        size: size.value,
+        sort
     };
 };
 
