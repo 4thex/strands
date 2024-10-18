a7c27b183e99bfadff57beebe27af6b8e25ecd0c
Palle Cogburn <palle@4thex.com>
2024-10-18 06:37:01 +0200

    Rendering ticks and labels on the axes. Need to transform with scale(-1, 1) to flip the numbers, but it widens it?

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -9,13 +9,49 @@ const renderer = conf => {
         g = document.createElement("g");
         g.setAttribute("stroke-width", "0.05");
         g.setAttribute("transform", "matrix(1 0 0 -1 0 10)");
-        let tick = document.createElement("line");
-        tick.setAttribute("x1", "1");
-        tick.setAttribute("y1", "0");
-        tick.setAttribute("x2", "1");
-        tick.setAttribute("y2", "0.1");
-        g.appendChild(tick);
+        setTicks(document, g);
+        setLabels(document, g);
         svg.appendChild(g);    
+    };
+
+    const setLabels = (document, element) => {
+        for(let x=1; x<10; x++) {
+            renderLabel(document, element, x, 0.5, x);
+        }
+        for(let y=1; y<10; y++) {
+            renderLabel(document, element, 0.5, y, y);
+        }
+    };
+
+    const renderLabel = (document, element, x, y, text) => {
+        console.log(`y=${y}`);
+        let label = document.createElement('text');
+        label.setAttribute("x", x);
+        label.setAttribute("y", y);
+        label.setAttribute("fill", "black");
+        label.setAttribute("stroke-width", "0.01");
+        label.setAttribute("style", "font-size: 0.5");
+        label.setAttribute("transform", `rotate(180, ${x}, ${y})`);
+        label.innerHTML = text;
+        element.appendChild(label);
+    };
+
+    const setTicks = (document, element) => {
+        for(let x=1; x<10; x++) {
+            renderTick(document, element, x, 0, x, 0.1);
+        }
+        for(let y=1; y<10; y++) {
+            renderTick(document, element, 0, y, 0.1, y);
+        }
+    };
+
+    const renderTick = (document, element, x1, y1, x2, y2) => {
+        let tick = document.createElement("line");
+        tick.setAttribute("x1", x1);
+        tick.setAttribute("y1", y1);
+        tick.setAttribute("x2", x2);
+        tick.setAttribute("y2", y2);
+        element.appendChild(tick);
     };
  
     const render = (vectors) => {
